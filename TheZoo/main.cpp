db8250dcc8cecc1e700c0ccaaa429a6bc70fff2b
Юля <@DESKTOP-LDN2BMM>
2025-08-16 14:31:56 +0300

    Virtual Animal

---
+++ TheZoo/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+class Animal
+{
+public:
+	virtual void Sound() = 0;//чисто-виртуальный метод = 0
+};
+class Cat: public Animal{};
+
+class Lion : public Cat
+{
+public:
+	void Sound()override
+	{
+		cout << "PPPPPPP" << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//Animal animal; ошибки 4326 и 2259 - нельзя создать экземпляр 
+	Lion lion;
+}
