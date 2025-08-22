b80a083fdc4822ec018ec94f8f37b3a60e756a17
Юля <@DESKTOP-LDN2BMM>
2025-08-22 20:26:45 +0300

    encoding animal

--- TheZoo/main.cpp
+++ TheZoo/main.cpp
@@ -4,12 +4,17 @@ using namespace std;
 class Animal
 {
 public:
+	//Абстрактный класс
 	virtual void Sound() = 0;//чисто-виртуальный метод = 0
 };
-class Cat: public Animal{};
-
-class Lion : public Cat
+class Cat: public Animal
 {
+	//Абстрактный класс
+};
+
+class Lion: public Cat
+{
+	//Конкретный класс
 public:
 	void Sound()override
 	{
